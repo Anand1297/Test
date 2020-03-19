ac302c93c4aa13fe21c8bfb28ed4a618ea53ec5d
Anand1297 <anandgadiya08@gmail.com>
2020-03-19 11:41:15 +0530

    added newUser function and updated updateArray function

--- controllers/post.js
+++ controllers/post.js
@@ -4,16 +4,16 @@ const mongoose= require('mongoose');
 
 const getPosts=(req,res)=>{
    // res.send("Hello");
-   const posts= Post.find({},{_id:0, __v:0})
+   const posts= Post.post.find({},{_id:0, __v:0})
    .then(posts=>{
        res.send(posts);
-     //  res.status(200).json({posts:posts});
+      //res.status(200).json({posts:posts});
    })
     .catch(err =>console.log(err));
 };
 
 const createPost= (req,res)=>{
-const post = new Post(req.body);
+const post = new Post.post(req.body);
 console.log(post);
 post.save((err,result)=>{
 if(err){
@@ -30,7 +30,7 @@ res.status(200).json({
 const getPostById= async (req,res,next)=>{
     const id= req.params.id;
     try{
-    const post=await Post.findById(id);
+    const post=await Post.post.findById(id).populate("id");
     if(!post){
         throw createError(404,"Post not Found");
     }
@@ -46,20 +46,30 @@ res.send(post);
 };
 
 const updateArray= async(req,res,next)=>{
+try{
 const id= req.body.id;
 const result= req.body.marks;
-console.log(id+" "+result)
-
-try{
-const post=await Post.updateOne({_id:id},{$addToSet:{Subject:[{marks:result}]}})
+console.log(id+" "+result);
+const post=await Post.post.findOneAndUpdate({id:id},{$addToSet:{Subject:[{marks:result}]}},{new:true});
 res.send(post);
 }catch(err){
-res.send(err);
+res.send(err.message);
+}
 }
 
+const AddUser=async (req,res,next)=>{
+    try{
+const newUser= new Post.user(req.body);
+const result=await newUser.save();
+//const id={"id":result._id};
+res.send(result);
+    }catch(error){
+        console.log(error.message);
+    }
 }
 
 module.exports.getPosts=getPosts;
 module.exports.createPost= createPost;
 module.exports.getPostById=getPostById;
 module.exports.updateArray=updateArray;
+module.exports.AddUser=AddUser;
