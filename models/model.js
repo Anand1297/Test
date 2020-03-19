e4f040766036b3d373f9a590a1706db545e32157
Anand1297 <anandgadiya08@gmail.com>
2020-03-19 11:17:30 +0530

    created new model and added new routes

--- models/model.js
+++ models/model.js
@@ -2,18 +2,22 @@ const mongoose= require('mongoose');
 const data= new mongoose.Schema({
     title: {
 type :String,
-required: true,
 minlength: 10,
 maxlength: 100
 },
     body :{
 type :String,
-required :true,
 minlength:10,
 maxlength:100
 },
-Subject:[{marks:{type:Number}}]
+Subject:[{marks:{type:Number}}],
+id:{type:mongoose.Schema.Types.ObjectId,ref:'User'}
 });
 
-module.exports= mongoose.model("Demo",data);
+const user= new mongoose.Schema({
+ name:{type:String},
+ phone:{type:Number}   
+});
 
+module.exports.post= mongoose.model("Demo",data);
+module.exports.user=mongoose.model("User",user);
--- routes/post.js
+++ routes/post.js
@@ -2,12 +2,19 @@ const express= require('express');
 const postControllers= require('../controllers/post')
  
 const router = express.Router();
-router.get('/',postControllers.getPosts);
+//to get all post
+router.get('/getpost',postControllers.getPosts);
 
+//to create post
 router.post('/post',postControllers.createPost);
 
+//to get post by ID
 router.get('/getpost/:id',postControllers.getPostById);
 
+//to update array
 router.post('/updateArray',postControllers.updateArray);
+
+//tp add new user
+router.post('/Adduser',postControllers.AddUser);
 
 module.exports= router;
